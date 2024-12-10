153c95d34ca56ad864f1692da32b06160287c892
Chinseok Lee <me@pyhub.kr>
2024-12-10 12:26:38 +0900

    장고 5에서 제거된 GET 요청에 의한 로그아웃 지원 (비추천)

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
+from django import __version__
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.version import get_version_tuple
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from accounts.forms import SignupForm, LoginForm
-
 
 login = auth_views.LoginView.as_view(
     form_class=LoginForm,
@@ -14,7 +15,16 @@ login = auth_views.LoginView.as_view(
 )
 
 
-logout = auth_views.LogoutView.as_view(
+class LogoutView(auth_views.LogoutView):
+    # 장고 5.0에서 제거된 GET 요청에 의한 로그아웃 지원 (비추천)
+    if get_version_tuple(__version__) >= (5, 0, 0):
+        http_method_names = ["get"] + list(auth_views.LogoutView.http_method_names)
+
+        def get(self, request, *args, **kwargs):
+            return self.post(request, *args, **kwargs)
+
+
+logout = LogoutView.as_view(
     next_page="accounts:login",
 )
 
