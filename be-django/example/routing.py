3f0447dda265f2031505a1b92f60e33f243f0637
Chinseok Lee <me@pyhub.kr>
2024-12-10 22:45:34 +0900

    웹소켓 비활성화

--- be-django/example/routing.py
+++ be-django/example/routing.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .agents import SituationChatView
-from .consumers import SituationChatConsumer
+
+# from .consumers import SituationChatConsumer
 
 
 websocket_urlpatterns = [
-    path("ws/example/agent/chat/<int:pk>/", SituationChatConsumer.as_asgi()),
+    # path("ws/example/agent/chat/<int:pk>/", SituationChatConsumer.as_asgi()),
 ]
 
 sse_urlpatterns = [
