cfc76c4d08bfff550f67c496522fed2d7ab87e83
Chinseok Lee <me@pyhub.kr>
2024-12-12 10:46:06 +0900

    ChatRoom 디폴트 LLM 모델을 anthropic claude 3.5 haiku로 변경

--- be-django/example/models.py
+++ be-django/example/models.py
@@ -16,7 +16,7 @@ class ChatRoom(LifecycleModel):
     llm_model = models.CharField(
         max_length=50,
         choices=[(model.name, model.name) for model in LLMModel],
-        default=LLMModel.OPENAI_GPT_4O.name,
+        default=LLMModel.ANTHROPIC_CLAUDE_3_5_HAIKU.name,
     )
     llm_temperature = models.FloatField(
         default=1, validators=[MinValueValidator(0), MaxValueValidator(1)]
