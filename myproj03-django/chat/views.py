c31d91b505fc98b14537bf07b2c74e6b0c22c4e2
Chinseok Lee <me@pyhub.kr>
2024-12-28 22:02:02 +0900

    openai 라이브러리를 활용하여 채팅 응답 생성

--- myproj03-django/chat/views.py
+++ myproj03-django/chat/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.html import format_html
+from openai import OpenAI
 
 
 def index(request):
@@ -11,8 +12,22 @@ def reply(request):
 
     if request.method == "POST":
         human_content = request.POST.get("content", "")
-        ai_content = f"입력하신 메시지는 {len(human_content)} 글자입니다."
 
-        return HttpResponse(format_html("<div>[Human] {}</div><div>[AI] {}</div>", human_content, ai_content));
+        client = OpenAI()
+
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "당신은 친절한 AI 어시스턴트입니다."},
+                {"role": "user", "content": human_content},
+            ],
+        )
+        ai_content = completion.choices[0].message.content
+
+        return HttpResponse(
+            format_html(
+                "<div>[Human] {}</div><div>[AI] {}</div>", human_content, ai_content
+            )
+        )
     else:
         return HttpResponse("<div>허용하지 않는 메서드</div>")
