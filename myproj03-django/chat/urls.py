6ca8a11146b3d89abc3b4edcf96c7094689cde8a
Chinseok Lee <me@pyhub.kr>
2024-12-28 21:53:18 +0900

    vanilla html form/js 기반으로 간단 채팅 UI 구현

--- myproj03-django/chat/urls.py
+++ myproj03-django/chat/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name = 'chat'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('reply/', views.reply, name='reply'),
+]
--- myproj03-django/chat/views.py
+++ myproj03-django/chat/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.html import format_html
 
-# Create your views here.
+
+def index(request):
+    return render(request, "chat/index.html")
+
+
+def reply(request):
+
+    if request.method == "POST":
+        human_content = request.POST.get("content", "")
+        ai_content = f"입력하신 메시지는 {len(human_content)} 글자입니다."
+
+        return HttpResponse(format_html("<div>[Human] {}</div><div>[AI] {}</div>", human_content, ai_content));
+    else:
+        return HttpResponse("<div>허용하지 않는 메서드</div>")
