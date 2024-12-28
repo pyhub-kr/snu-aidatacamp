465d4e5e7a339610c474ed23ace6353ff4755ee5
Chinseok Lee <me@pyhub.kr>
2024-12-28 22:08:23 +0900

    세션을 활용한 채팅 메시지 저장 및 활용

--- myproj03-django/chat/views.py
+++ myproj03-django/chat/views.py
@@ -5,24 +5,35 @@ from openai import OpenAI
 
 
 def index(request):
-    return render(request, "chat/index.html")
+    chat_messages = request.session.get('chat_messages', [])
+    return render(request, "chat/index.html", {
+        "chat_messages": chat_messages,
+    })
 
 
 def reply(request):
 
     if request.method == "POST":
+        # TODO: 값에 대한 유효성 검사는 장고 Form을 활용하면 편리.
         human_content = request.POST.get("content", "")
+
+        chat_messages = request.session.get('chat_messages', [])
+        if not chat_messages:
+            chat_messages = [
+                {"role": "system", "content": "당신은 친절한 AI 어시스턴트입니다."},
+            ]
+        chat_messages.append({"role": "user", "content": human_content})
 
         client = OpenAI()
 
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": "당신은 친절한 AI 어시스턴트입니다."},
-                {"role": "user", "content": human_content},
-            ],
+            messages=chat_messages,
         )
         ai_content = completion.choices[0].message.content
+        chat_messages.append({ "role": "assistant", "content": ai_content })
+
+        request.session['chat_messages'] = chat_messages
 
         return HttpResponse(
             format_html(
